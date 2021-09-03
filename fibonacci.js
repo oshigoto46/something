ef615f8f39981fabef6a514b8543d447ff998bd0
oshigoto46 <ppp.shiro46@gmail.com>
2021-09-03 21:24:52 +0300

    refactoring and perfomance imporvement

--- fibonacci.js
+++ fibonacci.js
@@ -1,18 +1,14 @@
 /**
- * @param  {number} targetNum
- * @returns {boolean} - whether prune number or not
+ * @param  {number}   targetNum
+ * @returns {boolean}  whether prime number or not
  */
 
-function isPruneNum(targetNum) {
+function isPrimeNum(targetNum) {
   // better to cut off odds number from performance viewpoint
   // odds number should not be calculated
   if (targetNum % 2 === 0) return false;
 
   // better to cut off calculating over Math.sqrt(num) from performance viewpoint
-  // ex. in case of 120 (can be divide into 10 * 12 )
-  // 12(> Math.sqrt(120)) would be unnecessary to be calculated
-  // because already 10 is appeared and suggests its  a element who can divide 120
-
   for (let i = 2; i < Math.sqrt(targetNum); i++) {
     if (targetNum % i === 0) return false;
   }
@@ -22,7 +18,7 @@ function isPruneNum(targetNum) {
 
 // /**
 //  * @param  {number} index
-//  * @return {number} - fibonacci number
+//  * @return {number} fibonacci number
 //  */
 
 // function fibonacci(index) {
@@ -34,7 +30,7 @@ function isPruneNum(targetNum) {
 
 /**
  * @param  {number}  targetNum
- * @return {number}  the minimum fibonacci & prune number over targetNum
+ * @return {number}  the minimum fibonacci & prime number over targetNum
  */
 
 function nxtPrmFib(targetNum) {
@@ -49,8 +45,8 @@ function nxtPrmFib(targetNum) {
     fibonacciCache[0] = fibonacciCache[1];
     fibonacciCache[1] = tmp + fibonacciCache[1];
 
-    if (fibonacciCache[1] >= targetNum) {
-      if (isPruneNum(fibonacciCache[1])) {
+    if (fibonacciCache[1] > targetNum) {
+      if (isPrimeNum(fibonacciCache[1])) {
         break;
       }
     }
